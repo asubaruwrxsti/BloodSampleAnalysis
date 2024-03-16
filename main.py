840bcfe3c6df541a662725498dd6bbdc8c059d4e
asubaruwrxsti <arlind12344@gmail.com>
2024-03-16 21:32:47 +0100

    Small fixes

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from database import Database
+from utils.populateDatabase import populateParameters, populateDiseaseNames
 
 # Create a database object
 db = Database('db.sqlite3')
+
+if __name__ == "__main__":
+    # Populate the database with the data from the dataset
+    populateParameters(db, 'Blood_samples_dataset_balanced_2(f).csv', exclude=['Disease'])
+
+    # Populate the database with the disease names
+    populateDiseaseNames(db, 'Blood_samples_dataset_balanced_2(f).csv')
--- utils/populateDatabase.py
+++ utils/populateDatabase.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from database import Database
 
-# Insert the data into the database
-def populateDatabase(db: Database, dataset: str, exclude: list = []):
+# Populate the database with the parameter names
+def populateParameters(db: Database, dataset: str, exclude: list = []):
     try:
         # Load the data
         data = pd.read_csv(dataset)
@@ -12,14 +12,34 @@ def populateDatabase(db: Database, dataset: str, exclude: list = []):
 
         for column in data.columns:
             # Check if the column name already exists in the database
-            select_query = "SELECT * FROM diseases WHERE name = ?"
+            select_query = "SELECT * FROM parameters WHERE name = ?"
             db.cursor.execute(select_query, (column,))
             result = db.cursor.fetchone()
 
             # If the column name doesn't exist in the database, insert it
             if result is None:
-                insert_query = "INSERT INTO diseases (name) VALUES (?)"
+                insert_query = "INSERT INTO parameters (name) VALUES (?)"
                 db.execute_query(insert_query, (column,))
+    
+    except Exception as e:
+        print(e)
+
+# Populate the database with the disease names
+def populateDiseaseNames(db: Database, dataset: str):
+    try:
+        # Load the data
+        data = pd.read_csv(dataset)
+
+        for disease in data['Disease'].unique():
+            # Check if the disease name already exists in the database
+            select_query = "SELECT * FROM diseases WHERE name = ?"
+            db.cursor.execute(select_query, (disease,))
+            result = db.cursor.fetchone()
+
+            # If the disease name doesn't exist in the database, insert it
+            if result is None:
+                insert_query = "INSERT INTO diseases (name) VALUES (?)"
+                db.execute_query(insert_query, (disease,))
     
     except Exception as e:
         print(e)
