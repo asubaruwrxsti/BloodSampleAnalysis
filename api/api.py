5fb2144b16b46d62c026c9a1cd61eefaba044bb2
asubaruwrxsti <arlind12344@gmail.com>
2024-03-19 21:08:04 +0100

    Improved structure

--- api/api.py
+++ api/api.py
@@ -1,43 +1,33 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, Depends
+from pydantic import ValidationError
 from utils.populateDatabase import populateDatabase
 from utils.predictor import trainPredictor
-from database import Database
 from utils.models import BloodSample
+from database import Database
 
 class API:
-    def __init__(self, DATA_FILE='Blood_samples_dataset_balanced_2(f).csv'):
-        """
-        Initialize the API instance.
-        
-        Parameters:
-        DATA_FILE (str): The path to the data file. Default is 'Blood_samples_dataset_balanced_2(f).csv'.
-        """
-        self.app = FastAPI()
-        self.db = Database('db.sqlite3')
-        self.data_file = DATA_FILE
-        self.predictor = trainPredictor(DATA_FILE)
-
-        populateDatabase(self.db, DATA_FILE)
+    def __init__(self, app: FastAPI, db: Database, predictor):
+        self.app = app
+        self.db = db
+        self.predictor = predictor
 
     def configure(self):
-        """
-        Configure the API routes.
-        """
         @self.app.post("/")
         async def predict(request: Request):
-            """
-            Predict the disease based on the blood sample data.
-            
-            Parameters:
-            request (Request): The request object containing the blood sample data.
-            
-            Returns:
-            dict: A dictionary containing the predicted disease.
-            """
-            data = await request.json()
-            blood_sample = BloodSample(**data)
-            return {"Disease": self.predictor.predict(blood_sample.getData())}
+            try:
+                data = await request.json()
+                blood_sample = BloodSample(**data)
+                return {"Disease": self.predictor.predict(blood_sample.getData())}
+            except ValidationError as e:
+                raise HTTPException(status_code=400, detail=e.json())
 
-api_instance = API()
-api_instance.configure()
-app = api_instance.app
+def create_api(db: Database, predictor, data_file='Blood_samples_dataset_balanced_2(f).csv'):
+    app = FastAPI()
+    populateDatabase(db, data_file)
+    api = API(app, db, predictor)
+    api.configure()
+    return app
+
+db = Database('db.sqlite3')
+predictor = trainPredictor('Blood_samples_dataset_balanced_2(f).csv')
+app = create_api(db, predictor)
